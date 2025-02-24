9578d10ba8da03d0e45efc18c567d6f2aeac97a9
Sedong <bulssajo00@gmail.com>
2025-02-24 15:01:45 +0900

    fix: hydration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import React from "react";
+import React, { Suspense } from "react";
 
-import { KanbanBoard } from "@/components/KanbanBoard";
-import { useBoardStore } from "@/hooks/useBoardStore";
-import { BoardDataMap } from "@/utils/reorder";
+
+
+import dynamic from "next/dynamic";
+const KanbanBoard = dynamic(() => import("@/components/KanbanBoard"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="h-full overflow-auto container mx-auto">
-      <KanbanBoard />
+    <div className="h-full overflow-auto container mx-auto" suppressHydrationWarning>
+      <Suspense fallback={<div>Loading...</div>}>
+        <KanbanBoard />
+      </Suspense>
     </div>
   )
 }
